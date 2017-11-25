1ccef7c386ac71b10d4330f9f34b51d135a3ac2c
Vishesh <vishesh.2710@gmail.com>
2017-11-25 19:58:20 +0530

    Write docs for visibleToAvoidSynthetics annotation

--- metrics/src/main/java/com/facebook/battery/metrics/core/VisibleToAvoidSynthetics.java
+++ metrics/src/main/java/com/facebook/battery/metrics/core/VisibleToAvoidSynthetics.java
@@ -6,6 +6,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Indicates that the given field, method, class, interface or enum has package private visibility
+ * solely to prevent creation of synthetic accessors and that the visibility of members with this
+ * annotation shouldn't be changed from package private.
+ */
 @Retention(RetentionPolicy.SOURCE)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.TYPE})
 public @interface VisibleToAvoidSynthetics {
