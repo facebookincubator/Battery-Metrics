35eddd8d7750483230e898f96380391e368b5710
Kunal Bhalla <kunalb@fb.com>
2018-10-03 11:30:25 -0700

    Fix CompositeMetricsReporterTest
    Summary: This is slightly embarrassing: this test has been broken for quite some time.
    
    Differential Revision: D10159064
    
    fbshipit-source-id: e5a23932e5c24a408a5e88520a94c13511f05042

--- reporters/src/test/java/com/facebook/battery/reporter/composite/CompositeMetricsReporterTest.java
+++ reporters/src/test/java/com/facebook/battery/reporter/composite/CompositeMetricsReporterTest.java
@@ -12,6 +12,7 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import com.facebook.battery.metrics.composite.CompositeMetrics;
 import com.facebook.battery.metrics.core.ReporterEvent;
 import com.facebook.battery.metrics.time.TimeMetrics;
+import com.facebook.battery.reporter.time.TimeMetricsReporter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,13 +27,16 @@ public class CompositeMetricsReporterTest {
   @Before
   public void setUp() {
     mReporter = new CompositeMetricsReporter();
+    mReporter.addMetricsReporter(TimeMetrics.class, new TimeMetricsReporter());
     mEvent = new ReporterEvent();
   }
 
   @Test
   public void testInvalidMetricsSkipped() {
-    CompositeMetrics metrics =
-        new CompositeMetrics().putMetric(TimeMetrics.class, new TimeMetrics());
+    TimeMetrics timeMetrics = new TimeMetrics();
+    timeMetrics.realtimeMs = 100;
+    timeMetrics.uptimeMs = 10;
+    CompositeMetrics metrics = new CompositeMetrics().putMetric(TimeMetrics.class, timeMetrics);
     metrics.setIsValid(TimeMetrics.class, false);
 
     mReporter.reportTo(metrics, mEvent);
@@ -41,11 +45,13 @@ public class CompositeMetricsReporterTest {
 
   @Test
   public void testValidMetricsLogged() {
-    CompositeMetrics metrics =
-        new CompositeMetrics().putMetric(TimeMetrics.class, new TimeMetrics());
-    metrics.setIsValid(TimeMetrics.class, false);
+    TimeMetrics timeMetrics = new TimeMetrics();
+    timeMetrics.realtimeMs = 100;
+    timeMetrics.uptimeMs = 10;
+    CompositeMetrics metrics = new CompositeMetrics().putMetric(TimeMetrics.class, timeMetrics);
+    metrics.setIsValid(TimeMetrics.class, true);
 
     mReporter.reportTo(metrics, mEvent);
-    assertThat(mEvent.eventMap.size()).isEqualTo(0);
+    assertThat(mEvent.eventMap.size()).isEqualTo(2);
   }
 }
