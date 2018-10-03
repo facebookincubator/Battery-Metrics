fee459eab4c074132513248fbdafcc44c76ffb76
Kunal Bhalla <kunalb@fb.com>
2018-10-02 18:21:50 -0700

    Fix headers
    Summary: TSIA.
    
    Differential Revision: D10151404
    
    fbshipit-source-id: 740097f9254ba3bb53f8255493a659b21cc5971c

--- metrics/src/main/java/com/facebook/battery/metrics/healthstats/HealthStatsMetrics.java
+++ metrics/src/main/java/com/facebook/battery/metrics/healthstats/HealthStatsMetrics.java
@@ -1,5 +1,10 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
+/*
+ * Copyright (c) 2017-present, Facebook, Inc. All rights reserved.
+ *
+ * <p>This source code is licensed under the BSD-style license found in the LICENSE file in the root
+ * directory of this source tree. An additional grant of patent rights can be found in the PATENTS
+ * file in the same directory.
+ */
 package com.facebook.battery.metrics.healthstats;
 
 import android.os.Build;
