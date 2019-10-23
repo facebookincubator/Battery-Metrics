5f17e2ec1e7736a1a59d7257f57e7a64cee703b0
Andres Suarez <asuarez@fb.com>
2019-10-22 17:15:55 -0700

    Yearless license header
    Differential Revision: D18072256
    
    fbshipit-source-id: e83f66f1bca222385534631967cdcb1c0f8009d7

--- metrics/src/test/java/com/facebook/battery/metrics/bluetooth/BluetoothMetricsTest.java
+++ metrics/src/test/java/com/facebook/battery/metrics/bluetooth/BluetoothMetricsTest.java
@@ -1,9 +1,10 @@
-/**
+/*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
+
 package com.facebook.battery.metrics.bluetooth;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
