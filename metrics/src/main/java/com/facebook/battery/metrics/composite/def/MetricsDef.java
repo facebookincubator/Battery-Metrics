9dd9e009e29cde6848825b50e4bc4abf686a2d96
Josh Guilfoyle <devjasta@fb.com>
2021-05-07 18:11:40 -0700

    Streamline configuration API to reduce boilerplate
    Summary:
    This boilerplate at best was awkward, at worst a good source of mistakes.
    This is a pure refactor that does not intend to change the behaviour of the
    code at all.
    
    Differential Revision: D28299235
    
    fbshipit-source-id: e4a1010759446294e3be6828e90e702ee7de03e1

---
+++ metrics/src/main/java/com/facebook/battery/metrics/composite/def/MetricsDef.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.battery.metrics.composite.def;
+
+import com.facebook.battery.metrics.core.SystemMetrics;
+import com.facebook.battery.metrics.core.SystemMetricsCollector;
+import com.facebook.battery.reporter.core.SystemMetricsReporter;
+import com.facebook.battery.serializer.core.SystemMetricsSerializer;
+
+/**
+ * Class that binds a {@link SystemMetrics} class to its respective driver classes. See {@link
+ * CompositeMetricsHolder}.
+ */
+public class MetricsDef<T extends SystemMetrics<T>> {
+  public final Class<T> clazz;
+  public final SystemMetricsCollector<T> collector;
+  public final SystemMetricsReporter<T> reporter;
+  public final SystemMetricsSerializer<T> serializer;
+  public final boolean isNonZeroAtColdStart;
+
+  public MetricsDef(
+      Class<T> clazz,
+      SystemMetricsCollector<T> collector,
+      SystemMetricsReporter<T> reporter,
+      SystemMetricsSerializer<T> serializer,
+      boolean isNonZeroAtColdStart) {
+    this.clazz = clazz;
+    this.collector = collector;
+    this.reporter = reporter;
+    this.serializer = serializer;
+    this.isNonZeroAtColdStart = isNonZeroAtColdStart;
+  }
+}
